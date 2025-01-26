743076c9d62c6a2f595305e3d44807e9766d6698
Sever <html.html.js@gmail.com>
2025-01-26 15:19:41 +0300

    First lesson

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4*a*c;
   
+  if(d > 0) {
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  } else if (d === 0) {
+    arr[0] = -b / (2*a);
+  } 
   return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let monthlyPercent = percent / 1200;
+  let bodyCredit =  amount - contribution;
+
+  let payMonthly = bodyCredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths)-1)));
+  let payAll = payMonthly * countMonths;
   
+  return Number(payAll.toFixed(2));
 }
