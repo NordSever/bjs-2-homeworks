9ad18858b71a60b7b1c0cbc0446932ccb7a39a5c
Sever <html.html.js@gmail.com>
2025-02-10 16:31:12 +0300

    Sixth lesson

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(strNumber) {
+  let resultParse = Number.parseFloat(strNumber);
+  if (Number.isNaN(resultParse)) {
+    throw new Error('Невалидное значение');
+  }
+    return resultParse;
+}
+
+function validateCount(strNumber2) {
+  try {
+    return parseCount(strNumber2);
+  } catch(err){
+    return err;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if(b + c < a || a + c < b || a + b < c) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    let p = 0.5 * (this.a + this.b + this.c);
+    return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch(err) {
+      return {
+        get perimeter() {
+          return 'Ошибка! Треугольник не существует';
+        },
+        get area() {
+          return 'Ошибка! Треугольник не существует';
+        }
+      }
+  }
+}      
+    
+
